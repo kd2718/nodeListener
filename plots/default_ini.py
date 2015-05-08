979667af2964c55c3ba76a7ca61ed7ea782b8463
kd2718 <kd2718@outlook.com>
2015-05-08 00:31:47 -0700

    redue plot tool to be a serveice

---
+++ plots/default_ini.py
@@ -0,0 +1,12 @@
+ini = {
+       'xscale', '20'
+       'ymax', '100'
+       'ymin', '-1'
+       'title', 'This is the plot Title'
+       'xlabel', 'this is the x axis label'
+       'ylabel', 'this is the y axis label'
+       'color', 'red'
+       'output_location', ''
+       'output_file', 'name.png'
+       'data_in', 'in.csv'
+}
